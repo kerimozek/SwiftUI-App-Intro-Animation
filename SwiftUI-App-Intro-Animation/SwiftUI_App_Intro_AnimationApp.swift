073e713ef52ab73f502ade25af6a982a916e03b8
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2023-01-09 16:56:40 +0300

    first commit

---
+++ SwiftUI-App-Intro-Animation/SwiftUI_App_Intro_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_App_Intro_AnimationApp.swift
+//  SwiftUI-App-Intro-Animation
+//
+//  Created by Mehmet Kerim ÖZEK on 9.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_App_Intro_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
